fa095429453f61ecc2b3c9c7597d89540bc88c27
Xavier Thomas <xavier.thomas.1980@gmail.com>
2021-07-12 02:24:12 -0300

    Bash: Add Arch Liunux maintenance script (still early but usable)

---
+++ .bash/arch-maintenance.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/bash
+
+check_errors_sys () {
+    systemctl --failed
+}
+
+check_errors_journal () {
+    journalctl -p 3 -b
+}
+
+upgrade_system () {
+    yay -Syu
+}
+
+upgrade_system_config () {
+    pacdiff
+}
+
+clean_package_cache () {
+    sudo paccache -r
+}
+
+remove_orphan_packages () {
+    pacman -Qtdq | sudo pacman -Rns -
+}
+
+
+find_broken_links () {
+    sudo find / -xdev -xtype l -print
+    # find /home -xdev -xtype l -print
+}
+
+reboot_system () {
+    read -p "Are you sure? (y/n) " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        sudo systemctl reboot
+    fi
+}
+
+options=(
+    "Check for system errors"
+    "Check for errors in journal"
+    "Upgrade system packages"
+    "Upgrade system config (pacdiff)"
+    "Remove orphaned packages"
+    "Clean package cache"
+    # TODO old config files, rmshit, rmlint
+    "Find lost files"
+    "Find broken links"
+    "Reboot"
+)
+
+echo
+PS3="> "
+select opt in "${options[@]}" "Quit"; do 
+    echo
+    case "$REPLY" in
+    1) check_errors_sys;;
+    2) check_errors_journal;;
+    3) upgrade_system;;
+    4) upgrade_system_config;;
+    5) remove_orphan_packages;;
+    6) clean_package_cache;;
+    7) find_lost_files;;
+    8) find_broken_links;;
+    9) reboot_system;;
+    $((${#options[@]}+1))) echo ""; break;;
+    *) echo "Invalid option. Try again.";;
+    esac
+    echo
+    REPLY=
+done
