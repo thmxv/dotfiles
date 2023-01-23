8b40d7270c6908cbe6fe8a2db2fe42d018ed4c1f
Xavier Thomas <xavier.thomas.1980@gmail.com>
2023-01-23 00:33:14 -0300

    Neovim: Small cleanups

--- .config/nvim/lua/thmxvr/settings.lua
+++ .config/nvim/lua/thmxvr/settings.lua
@@ -27,22 +27,9 @@ vim.opt.listchars = {
   eol='⏎',
 }
 -- vim.opt.list = true              -- Display above hidden control char
--- vim.opt.showmatch = true            -- Show matching parenthesis
--- vim.opt.mouse = 'a'
--- 
--- vim.opt.hidden = true               -- Allow openning/switch buffer when current buffer is edited
--- --vim.opt.ignorecase = true         -- Ignore case for search
--- vim.opt.history = 50                -- Commands in search history
--- vim.opt.undolevels = 1000           -- Levels of undo
--- vim.opt.title = true                -- Change terminal's title
--- vim.opt.visualbell = true           -- No beep
--- --vim.opt.errorbells = false        -- No beep
--- vim.opt.cursorline = true
--- vim.opt.splitright = true
--- vim.opt.splitbelow = true
--- 
--- g.netrw_liststyle = 3
--- g.netrw_banner = 0
+vim.opt.cursorline = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 vim.cmd.filetype("indent plugin on")
 
@@ -58,10 +45,4 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Neovide specifics
 if vim.g.neovide == true then
   vim.o.guifont = 'JetBrainsMono Nerd Font:h14'
---   vim.keymap.set(
---     'n',
---     '<F11>',
---     ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>",
---     {}
---   )
 end
